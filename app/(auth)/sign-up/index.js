8f2ecba9734bac55197984bfcc82586c64d3f083
SPatil2026 <Samarthpatil2022.comp@mmcoe.edu.in>
2024-10-01 23:44:33 +0530

    Sign-in Sign-up Done

---
+++ app/(auth)/sign-up/index.js
@@ -0,0 +1,110 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation, useRouter } from 'expo-router'
+import { Colors } from './../../../constants/Colors'
+import { TouchableOpacity } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function SignUp() {
+  const navigation=useNavigation();
+  const router=useRouter();
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown:false
+    })
+  },[])
+  return (
+    <View style={{
+      padding:25,
+      paddingTop:40,
+      backgroundColor:Colors.WHITE,
+      height:'100%'
+    }}>
+      <TouchableOpacity onPress={()=>router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+
+      <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize:30,
+        marginTop:30
+      }}>Create New Account</Text>
+
+      {/* User full name*/}
+      <View style={{
+        marginTop:50
+      }}>
+        <Text style={{
+          fontFamily:"outfit"
+        }}>Full Name</Text>
+        <TextInput
+        style={styles.input} 
+        placeholder='Enter Full Name'/>
+      </View>
+
+      {/* Email */}
+      <View style={{
+        marginTop:20
+      }}>
+        <Text style={{
+          fontFamily:"outfit"
+        }}>Email</Text>
+        <TextInput
+        style={styles.input} 
+        placeholder='Enter Email'/>
+      </View>
+      
+      {/* Password */}
+      <View style={{
+        marginTop:20
+      }}>
+        <Text style={{
+          fontFamily:"outfit"
+        }}>Password</Text>
+        <TextInput
+        secureTextEntry={true}
+        style={styles.input} 
+        placeholder='Enter Password'/>
+      </View>
+
+      {/* Create account button */}
+      <View style={{
+        backgroundColor:Colors.PRIMARY,
+        padding:20,
+        borderRadius:15,
+        marginTop:50,
+      }}>
+        <Text style={{
+          color:Colors.WHITE,
+          textAlign:'center'
+        }}>Create Account</Text>
+      </View>
+
+      {/* Create Account button */}
+      <TouchableOpacity 
+      onPress={()=>router.replace('/(auth)/sign-in')}
+      style={{
+        backgroundColor:Colors.WHITE,
+        padding:20,
+        borderRadius:15,
+        marginTop:20,
+        borderWidth:1,
+      }}>
+        <Text style={{
+          color:Colors.PRIMARY,
+          textAlign:'center'
+        }}>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input:{
+    borderWidth:1,
+    padding:15,
+    borderRadius:15,
+    borderColor:Colors.GRAY,
+    fontFamily:"outfit"
+  }
+})
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,8 +1,11 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
 
 export default function Login() {
+
+    const router = useRouter();
   return (
     <View>
       <Image source={require('./../assets/images/login.jpg')}
@@ -28,14 +31,16 @@ export default function Login() {
             marginTop:20
         }}>Discover your next adventure effortlessly. Personalized itineraries at your fingertips. Travel smarter with AI-driven insights</Text>
 
-        <View style={styles.button}>
+        <TouchableOpacity style={styles.button}
+            onPress={()=>router.push('/(auth)/sign-in')}
+        >
             <Text style={{
                 color:Colors.WHITE,
                 textAlign:'center',
                 fontFamily:'outfit',
                 fontSize:17
-            }}>Sign-in with Google</Text>
-        </View>
+            }}>Get Started</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
